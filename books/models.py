9e3374228e416e9d9c21b997302d5cf49cdbce19
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2021-07-15 22:13:48 -0500

    Change model names

--- books/models.py
+++ books/models.py
@@ -3,21 +3,19 @@ from django.db import models
 # Create your models here.
 
 
-class PublishingHouseModel(models.Model):
+class PublishingHouse(models.Model):
     name = models.CharField(max_length=50)
     address = models.TextField()
 
 
-class AuthorModel(models.Model):
+class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
 
-class BookModel(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=50)
-    author = models.ManyToManyField(to=AuthorModel)
+    author = models.ManyToManyField(to=Author)
     publishing_year = models.DateField()
-    publishing_house = models.ForeignKey(
-        to=PublishingHouseModel, on_delete=models.PROTECT
-    )
+    publishing_house = models.ForeignKey(to=PublishingHouse, on_delete=models.PROTECT)
     pages = models.IntegerField()
