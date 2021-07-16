08e9749d1396b7d5bc2aa59d0ca437add69de4bc
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2021-07-15 21:15:34 -0500

    Add models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PublishingHouseModel(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+
+
+class AuthorModel(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class BookModel(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ManyToManyField(to=AuthorModel)
+    publishing_year = models.DateField()
+    publishing_house = models.ForeignKey(
+        to=PublishingHouseModel, on_delete=models.PROTECT
+    )
+    pages = models.IntegerField()
